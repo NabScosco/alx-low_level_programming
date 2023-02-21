572820d3c9eab3ab0f565f12de5fc0962c65d256
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-02-21 12:01:28 -0800

    Worked on Header file  and functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** This file prints putchar
+ * Returns to a new line and
+ * It then returns 0
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return 0;
+}
