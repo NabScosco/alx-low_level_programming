c1c285d1d94eaaf64f9ee906b5ea7699e73f25db
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-02-22 15:00:56 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers
+ * @i: First value
+ * @y: Second value
+ * Return: Returns the value of the addition
+ */
+
+int add(int i, int y)
+{
+	return (i + y);
+}
