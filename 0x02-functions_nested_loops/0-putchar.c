b551cfcdc97601986cb22159916e60f7f3f2fd0f
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-02-21 16:08:04 -0800

    Another function that prints alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * This file prints putchar
- * Returns to a new line and
- *
- * It then returns 0.
+ * main - prints putchar
+ * Returns: always 0
  */
 int main(void)
 {
