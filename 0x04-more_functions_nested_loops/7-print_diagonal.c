933b0bfb8d1c5b3962b8fd847c1980151c67a3c4
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-02-23 17:03:13 -0800

    Printing a diognal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,9 @@ void print_diagonal(int n)
 	{
 		for (j = 0; j < i; j++)
 		{
-			_putchar('\');
+			_putchar(' ');
 		}
-		_putchar(n);
+		_putchar('\\');
 		if (i < (n - 1))
 			_putchar('\n');
 	}
