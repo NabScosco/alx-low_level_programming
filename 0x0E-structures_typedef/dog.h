97d99cb627faa3cd6ca35bada900c6077d9f606a
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-03-20 16:56:56 -0700

    Defined a structure in a header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+ * struct dog -  Definition of a structure
+ * @name: A member of the structure
+ * @owner: A member of the structure
+ * @age: A member of the structure
+ */
+
+struct dog
+{
+	*char name;
+	*char owner;
+	float age;
+};
+
+#endif
