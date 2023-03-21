9fce6b7d3ed624acc9b6f782ec475494e9feebf5
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-03-20 21:31:56 -0700

    Defining neew typedef of struct, creating new struct dog, copying name and owner and freeing dogs allocation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define STRUCT_DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog -  Definition of a structure
  * @name: A member of the structure
  * @owner: A member of the structure
@@ -15,5 +20,8 @@ struct dog
 	float age;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
