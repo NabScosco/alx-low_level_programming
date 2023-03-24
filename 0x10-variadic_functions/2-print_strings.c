4f71271b6b4cf9ede6edacbd2d2fdee2e911fa8a
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-03-23 22:14:34 -0700

    Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,26 +10,12 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int i;
-	char *str;
+	unsigned int i = 0;
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-	{
-		str = va_arg(valist, char *);
-
-		if (str)
-			printf("%s", str);
-		else
-			printf("(nil)");
-
-		if (i < n - 1)
-			if (separator)
-				printf("%s", separator);
-	}
-
-	printf("\n");
+		printf("%s", va_arg(valist, char *));
 	va_end(valist);
 }
 
