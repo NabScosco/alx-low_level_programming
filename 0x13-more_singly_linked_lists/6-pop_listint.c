ca5545a2c6c79db8f38d96990155eba9002a3752
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-04-04 22:05:01 -0700

    singly linked list tasks

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,6 @@
  * pop_listint - deletes the head node of a listint_t linked list,
  * and returns the head node's data (n).
  * @head: pointer to pointer to the head node of the linked list.
- *
  * Return: the head node's data (n), or 0 if the linked list is empty.
  */
 int pop_listint(listint_t **head)
