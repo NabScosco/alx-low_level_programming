232a4b90bfa99ca5138b5b5bb8cdf38fc0ffeac9
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-04-10 03:14:34 -0700

    Write a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1ul << (sizeof(n) * 8 - 1);
-
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
 
-	while (mask > 0)
+	while (n > 0)
 	{
-		_putchar((n & mask) ? '1' : '0');
-		mask >>= 1;
+		_putchar((n & 1) ? '1' : '0');
+		n >>= 1;
 	}
 }
