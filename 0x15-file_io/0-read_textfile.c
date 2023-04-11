a64a0ed56844e01dd767349d0b0abfc148658a48
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-04-10 22:18:22 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,19 +7,20 @@
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: the name of the file to read
  * @letters: the maximum number of characters to read and print
- * Return: the actual number of characters read and printed, or 0 if an error occurred
+ * Return: the actual number of characters read
+ * and printed, or 0 if an error occurred
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nread, nwritten, total = 0;
+	ssize_t n_read = 0, n_written = 0;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(letters);
+	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
 
@@ -30,30 +31,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	nread = read(fd, buf, letters);
-	if (nread == -1)
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
 	{
 		free(buf);
 		close(fd);
 		return (0);
 	}
 
-	while (nread > 0)
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1 || n_written != n_read)
 	{
-		nwritten = write(STDOUT_FILENO, buf + total, nread);
-		if (nwritten == -1)
-		{
-			free(buf);
-			close(fd);
-			return (0);
-		}
-
-		total += nwritten;
-		nread -= nwritten;
+		free(buf);
+		close(fd);
+		return (0);
 	}
 
 	free(buf);
 	close(fd);
-
-	return (total);
+	return (n_written);
 }
