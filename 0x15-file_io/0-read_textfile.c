b52388abfcd848915959780611de792c823e054f
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-04-10 22:12:32 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,59 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - This reads a text file and prints it to stdout
- * @filename: The name of the file to read
- * @letters: The number of letters to read from file
- * Return: The actual number of letters read and printed
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: the name of the file to read
+ * @letters: the maximum number of characters to read and print
+ * Return: the actual number of characters read and printed, or 0 if an error occurred
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, bytes_read, bytes_written;
-	char *buffer;
+	int fd;
+	ssize_t nread, nwritten, total = 0;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
 	{
+		free(buf);
+		return (0);
+	}
+
+	nread = read(fd, buf, letters);
+	if (nread == -1)
+	{
+		free(buf);
 		close(fd);
 		return (0);
 	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
+	while (nread > 0)
 	{
-		free(buffer);
-		close(fd);
-		return (0);
+		nwritten = write(STDOUT_FILENO, buf + total, nread);
+		if (nwritten == -1)
+		{
+			free(buf);
+			close(fd);
+			return (0);
+		}
+
+		total += nwritten;
+		nread -= nwritten;
 	}
 
-	buffer[bytes_read] = '\0';
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-	free(buffer);
+	free(buf);
 	close(fd);
-	return (bytes_read);
+
+	return (total);
 }
