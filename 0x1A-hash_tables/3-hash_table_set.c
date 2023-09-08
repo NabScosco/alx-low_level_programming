3dba1288aed1c69b1c254a761849581a9e01783b
Kingsley Fynn Asaam <kingsely.asaam@gmail.com>
2023-09-08 10:34:09 -0700

    changed the code in 3-hash_tabel_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,52 +1,61 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
- * hash_table_set - function that adds an element to the hash table
- * @ht: pointer to hash table
- * @key: key to add the element
- * @value: value to add the element
- *
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: the key. key can not be an empty string
+ * @value: the value associated with the key. value must be duplicated.
+ *         value can be an empty string
  * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	char *valuecopy, *keycopy;
-	hash_node_t  *bucket, *new_node;
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
 
-	if (!ht || !key || !*key || !value)
-		return (0);
-
-	valuecopy = strdup(value);
-	if (!valuecopy)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	bucket = ht->array[index];
+	current_node = ht->array[index];
 
-	while (bucket)
+	while (current_node != NULL)
 	{
-		if (!strcmp(key, bucket->key))
+		if (strcmp(current_node->key, key) == 0)
 		{
-			free(bucket->value);
-			bucket->value = valuecopy;
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
 			return (1);
 		}
-		bucket = bucket->next;
+		current_node = current_node->next;
 	}
-	new_node = calloc(1, sizeof(hash_node_t));
+
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		free(valuecopy);
+		free(new_node);
 		return (0);
 	}
-	keycopy = strdup(key);
-	if (!keycopy)
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
 		return (0);
-	new_node->key = keycopy;
-	new_node->value = valuecopy;
+	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
